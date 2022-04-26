eb8f61ab08ca4f59aba6597df78a4ec40e455e3d
Mosquera8 <70826423+Mosquera8@users.noreply.github.com>
2022-04-25 21:28:13 -0500

    prueba

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const taskRoutes = require('./routes/tasks.routes')
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(taskRoutes);
+
+app.listen(4000);
+console.log('Usa puerto 4000');
